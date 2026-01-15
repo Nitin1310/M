cb4b5045baaaeccb5ab607703ee7d232177ded2d
Nitin1310 <nitin.bhopatkar@gmail.com>
2026-01-15 19:47:37 +0530

    first commit

---
+++ head.php
@@ -0,0 +1,21 @@
+
+
+// Your code here
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Finazze - Business & Finance HTML Templete</title>
+
+  <!--=====FAB ICON=======-->
+  <link rel="shortcut icon" href="assets/img/logo/fav-logo1.png" type="image/x-icon">
+
+  <!--===== CSS LINK =======-->
+  <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/plugins/aos.css">
+  <link rel="stylesheet" href="assets/css/plugins/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+  <link rel="stylesheet" href="assets/css/plugins/owlcarousel.min.css">
+  <link rel="stylesheet" href="assets/css/plugins/sidebar.css">
+  <link rel="stylesheet" href="assets/css/plugins/slick-slider.css">
+  <link rel="stylesheet" href="assets/css/plugins/nice-select.css">
+  <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.css">
+  <link rel="stylesheet" href="assets/css/main.css">
---
+++ header.php
@@ -0,0 +1,165 @@
+
+<header class="homepage5-body">
+  <div id="vl-header-sticky" class="vl-header-area vl-transparent-header">
+      <div class="container">
+          <div class="row align-items-center">
+              <div class="col-lg-2 col-md-6 col-6">
+                  <div class="vl-logo">
+                      <a href="index.php"><img src="assets/img/logo/logo2.png" alt=""></a>
+                  </div>
+              </div>
+              <div class="col-lg-7 d-none d-lg-block">
+                  <div class="vl-main-menu text-center">
+                      <nav class="vl-mobile-menu-active">
+                          <ul>
+                              <li class="has-dropdown">
+                                  <a href="#">Home <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
+                                  <div class="vl-mega-menu">
+                                      <div class="vl-home-menu">                    
+                                          <div class="row gx-3 row-cols-1 row-cols-md-1 row-cols-lg-5">
+                                              <div class="col">
+                                                      <div class="vl-home-thumb">
+                                                          <div class="img1">
+                                                            <img src="assets/img/all-images/demo/demo-img1.png" alt="">
+                                                          </div>
+                                                         <a href="index.php">Finazze  - Homepage 01</a>
+                                                         <div class="btn-area1">
+                                                          <a href="index.php" class="vl-btn1">View Demo</a>
+
+                                                         </div>
+                                                         <div class="space20 d-lg-none d-block"></div>
+                                                      </div>
+                                              </div>
+                                              <div class="col">
+                                                      <div class="vl-home-thumb">
+                                                        <div class="img1">
+                                                          <img src="assets/img/all-images/demo/demo-img2.png" alt="">
+                                                        </div>
+                                                        <a href="index2.php">Finazze  - Homepage 02</a>
+                                                        <div class="btn-area1">
+                                                          <a href="index2.php" class="vl-btn1">View Demo</a>
+
+                                                         </div>
+                                                        <div class="space20 d-lg-none d-block"></div>
+                                                      </div>
+                                              </div>
+                                              <div class="col ">
+                                                      <div class="vl-home-thumb">
+                                                        <div class="img1">
+                                                          <img src="assets/img/all-images/demo/demo-img3.png" alt="">
+                                                        </div>
+                                                        <a href="index3.php">Finazze  - Homepage 03</a>
+                                                        <div class="btn-area1">
+                                                          <a href="index3.php" class="vl-btn1">View Demo</a>
+
+                                                         </div>
+                                                        <div class="space20 d-lg-none d-block"></div>
+                                                      </div>
+                                              </div>
+                                              <div class="col ">
+                                                      <div class="vl-home-thumb">
+                                                        <div class="img1">
+                                                          <img src="assets/img/all-images/demo/demo-img4.png" alt="">
+                                                        </div>
+                                                        <a href="index4.php">Finazze  - Homepage 04</a>
+                                                        <div class="btn-area1">
+                                                          <a href="index4.php" class="vl-btn1">View Demo</a>
+
+                                                         </div>
+                                                        <div class="space20 d-lg-none d-block"></div>
+                                                      </div>
+                                              </div>
+
+                                              <div class="col ">
+                                                    <div class="vl-home-thumb">
+                                                      <div class="img1">
+                                                        <img src="assets/img/all-images/demo/demo-img5.png" alt="">
+                                                      </div>
+                                                      <a href="index5.php">Finazze  - Homepage 05</a>
+                                                      <div class="btn-area1">
+                                                        <a href="index5.php" class="vl-btn1">View Demo</a>
+                                                       </div>
+                                                    </div>
+                                            </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </li>
+                              <li class="has-dropdown">
+                                <a href="#">Pages <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
+                                  <ul class="sub-menu">
+                                      <li><a href="about.php">About Us</a></li>
+                                      <li><a href="team.php">Our Team</a></li>
+                                      <li><a href="pricing.php">Pricing Plan</a></li>
+                                      <li><a href="testimonial.php">Testimonials</a></li>
+                                      <li><a href="contact.php">Contact Us</a></li>
+                                      <li><a href="faq.php">FAQ's</a></li>
+                                      <li><a href="404.php">404</a></li>
+                                  </ul>
+                              </li>
+                              <li><a href="#">Services <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
+                                <ul class="sub-menu">
+                                  <li><a href="service.php">Our Service</a></li>
+                                  <li><a href="#" class="span-arrow">Service Details <span><i class="fa-solid fa-angle-right d-lg-block d-none"></i></span></a>
+                                    <ul class="sub-menu menu1">
+                                      <li><a href="service-left.php">Service Left</a></li>
+                                      <li><a href="service-right.php">Service Right</a></li>
+                                      <li><a href="service-single.php">Service Single</a></li>
+                                    </ul>
+                                  </li>
+                              </ul>
+                              </li>
+                              <li><a href="#">Project <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
+                                <ul class="sub-menu">
+                                  <li><a href="project.php">Our Project</a></li>
+                                  <li><a href="#" class="span-arrow">Project  Details <span><i class="fa-solid fa-angle-right d-lg-block d-none"></i></span></a>
+                                    <ul class="sub-menu menu1">
+                                      <li><a href="project-left.php">Project Left</a></li>
+                                      <li><a href="project-right.php">Project Right</a></li>
+                                      <li><a href="project-single.php">Project Single</a></li>
+                                  </ul>
+                                  </li>
+                              </ul>
+                              </li>
+                              <li><a href="#">Blogs <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
+                                <ul class="sub-menu">
+                                  <li><a href="blog.php">Our Blog</a></li>
+                                  <li><a href="#" class="span-arrow">Blog Details <span><i class="fa-solid fa-angle-right d-lg-block d-none"></i></span></a>
+                                    <ul class="sub-menu menu1">
+                                      <li><a href="blog-left.php">Blog Left</a></li>
+                                      <li><a href="blog-right.php">Blog Right</a></li>
+                                      <li><a href="blog-single.php">Blog Single</a></li>
+                                  </ul>
+                                  </li>
+                              </ul>
+                              </li>
+
+                              <li><a href="contact.php">Contact Us</a></li>
+                          </ul>
+                      </nav>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-6 col-6">
+                <div class="vl-hero-btn d-none d-lg-block text-end">
+                  <div class="head-btn">
+                    <div class="search-icon header__search header-search-btn">
+                      <a href="#"><img src="assets/img/icons/search1.svg" alt=""></a>
+                    </div>
+                    <ul>
+                      <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                      <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                      <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                      <li><a href="#"><i class="fa-brands fa-pinterest-p"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+                  <div class="vl-header-action-item d-block d-lg-none">
+                      <button type="button" class="vl-offcanvas-toggle">
+                        <i class="fa-solid fa-bars-staggered"></i>
+                      </button>
+                   </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</header>
