9719fd1858017f9e78aa3453dccad875644b615d
Siddhant Deepak Pradhan <48193075+siddhant2202@users.noreply.github.com>
2026-01-28 16:09:01 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,170 +1,34 @@
-<header class="homepage1-body">
-  <div id="vl-header-sticky" class="vl-header-area vl-transparent-header">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-lg-2 col-md-6 col-6">
-          <div class="vl-logo">
-            <a href="index.html"><img src="assets/img/logo/1000252725.jpg" alt=""></a>
-          </div>
-        </div>
-        <div class="col-lg-7 d-none d-lg-block">
-          <div class="vl-main-menu text-center">
-            <nav class="vl-mobile-menu-active">
-              <ul>
-                <li><a href="index.php">Home</a></li>
-                <!-- <li class="has-dropdown">
-                  <a href="index.php">Home <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
-                  <div class="vl-mega-menu">
-                    <div class="vl-home-menu">
-                      <div class="row gx-3 row-cols-1 row-cols-md-1 row-cols-lg-5">
-                        <div class="col">
-                          <div class="vl-home-thumb">
-                            <div class="img1">
-                              <img src="assets/img/all-images/demo/demo-img1.png" alt="">
-                            </div>
-                            <a href="index.html">Finazze - Homepage 01</a>
-                            <div class="btn-area1">
-                              <a href="index.html" class="vl-btn1">View Demo</a>
+<header class="edu-header">
+  <div class="container">
+    <div class="edu-header-row">
 
-                            </div>
-                            <div class="space20 d-lg-none d-block"></div>
-                          </div>
-                        </div>
-                        <div class="col">
-                          <div class="vl-home-thumb">
-                            <div class="img1">
-                              <img src="assets/img/all-images/demo/demo-img2.png" alt="">
-                            </div>
-                            <a href="index2.html">Finazze - Homepage 02</a>
-                            <div class="btn-area1">
-                              <a href="index2.html" class="vl-btn1">View Demo</a>
-
-                            </div>
-                            <div class="space20 d-lg-none d-block"></div>
-                          </div>
-                        </div>
-                        <div class="col ">
-                          <div class="vl-home-thumb">
-                            <div class="img1">
-                              <img src="assets/img/all-images/demo/demo-img3.png" alt="">
-                            </div>
-                            <a href="index3.html">Finazze - Homepage 03</a>
-                            <div class="btn-area1">
-                              <a href="index3.html" class="vl-btn1">View Demo</a>
-
-                            </div>
-                            <div class="space20 d-lg-none d-block"></div>
-                          </div>
-                        </div>
-                        <div class="col ">
-                          <div class="vl-home-thumb">
-                            <div class="img1">
-                              <img src="assets/img/all-images/demo/demo-img4.png" alt="">
-                            </div>
-                            <a href="index4.html">Finazze - Homepage 04</a>
-                            <div class="btn-area1">
-                              <a href="index4.html" class="vl-btn1">View Demo</a>
-
-                            </div>
-                            <div class="space20 d-lg-none d-block"></div>
-                          </div>
-                        </div>
-
-                        <div class="col ">
-                          <div class="vl-home-thumb">
-                            <div class="img1">
-                              <img src="assets/img/all-images/demo/demo-img5.png" alt="">
-                            </div>
-                            <a href="index5.html">Finazze - Homepage 05</a>
-                            <div class="btn-area1">
-                              <a href="index5.html" class="vl-btn1">View Demo</a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li> -->
-                <li class="has-dropdown">
-                  <a href="#">Competitive Examination <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
-               <!--   <ul class="sub-menu">
-                    <li><a href="about.php">About Us</a></li>
-                    <li><a href="team.php">Our Team</a></li>
-                    <li><a href="pricing.php">Pricing Plan</a></li>
-                    <li><a href="testimonial.php">Testimonials</a></li>
-                    <li><a href="contact.php">Contact Us</a></li>
-                    <li><a href="faq.php">FAQ's</a></li>
-                    <li><a href="404.php">404</a></li>
-                  </ul> -->
-                </li> 
-                <li><a href="#">Graduation Courses <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
-                <!--  <ul class="sub-menu">
-                    <li><a href="service.php">Our Service</a></li>
-                    <li><a href="#" class="span-arrow">Service Details <span><i class="fa-solid fa-angle-right d-lg-block d-none"></i></span></a>
-                      <ul class="sub-menu menu1">
-                        <li><a href="service-left.php">Service Left</a></li>
-                        <li><a href="service-right.php">Service Right</a></li>
-                        <li><a href="service-single.php">Service Single</a></li>
-                      </ul>
-                    </li>
-                  </ul> -->
-                </li>
-                <li><a href="#">PG Courses <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
-              <!--    <ul class="sub-menu">
-                    <li><a href="project.php">Our Project</a></li>
-                    <li><a href="#" class="span-arrow">Project Details <span><i class="fa-solid fa-angle-right d-lg-block d-none"></i></span></a>
-                      <ul class="sub-menu menu1">
-                        <li><a href="project-left.php">Project Left</a></li>
-                        <li><a href="project-right.php">Project Right</a></li>
-                        <li><a href="project-single.php">Project Single</a></li>
-                      </ul>
-                    </li>
-                  </ul> -->
-                </li>
-                <li><a href="#">Study Abroad <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
-                </li>
-                <li><a href="#">Blogs <span><i class="fa-solid fa-angle-down d-lg-inline d-none"></i></span></a>
-               <!--   <ul class="sub-menu">
-                    <li><a href="blog.php">Our Blog</a></li>
-                    <li><a href="#" class="span-arrow">Blog Details <span><i class="fa-solid fa-angle-right d-lg-block d-none"></i></span></a>
-                      <ul class="sub-menu menu1">
-                        <li><a href="blog-left.php">Blog Left</a></li>
-                        <li><a href="blog-right.php">Blog Right</a></li>
-                        <li><a href="blog-single.php">Blog Single</a></li>
-                      </ul>
-                    </li>
-                  </ul> -->
-                </li>
-
-                <li><a href="contact.php">About Us</a></li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-6">
-          <div class="vl-hero-btn d-none d-lg-block text-end">
-            <div class="head-btn">
-              <div class="search-icon header__search header-search-btn">
-                <a href="#"><img src="assets/img/icons/search1.svg" alt=""></a>
-              </div>
-              <span class="vl-btn-wrap text-end">
-                <span class="icons">
-                  <a href="tel:+4909233255"><img src="assets/img/icons/phone1.svg" alt=""></a>
-                </span>
-                <span class="text">
-                  <span>Hotline 24/7</span>
-                  <a href="tel:+4909233255">+4930 9233255</a>
-                </span>
-              </span>
-            </div>
-          </div>
-          <div class="vl-header-action-item d-block d-lg-none">
-            <button type="button" class="vl-offcanvas-toggle">
-              <i class="fa-solid fa-bars-staggered"></i>
-            </button>
-          </div>
-        </div>
+      <!-- LOGO -->
+      <div class="edu-logo">
+        <a href="index.php">
+          <img src="assets/img/logo/1000252725-removebg-preview.png" alt="MEC Logo">
+        </a>
       </div>
+
+      <!-- MENU -->
+      <nav class="edu-menu">
+        <a href="index.php">Home</a>
+        <a href="#">Graduation Courses</a>
+        <a href="#">PG Courses</a>
+        <a href="#">Competitive Examination</a>
+        <a href="#">Study Abroad</a>
+        <a href="blog.php">Blogs</a>
+        <a href="about.php">About Us</a>
+      </nav>
+
+      <!-- HOTLINE -->
+      <div class="edu-hotline">
+  <span class="hotline-label">
+    <i class="fa-solid fa-phone"></i> Hotline 24/7
+  </span>
+  <a href="tel:+919028319393">+91 9028319393</a>
+</div>
+
+
     </div>
   </div>
 </header>
